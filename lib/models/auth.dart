690a9d8d544570dcf394bbae5a396a95a72ecdfa
Ritvik_Pramod <103210186+Ritvikadoor@users.noreply.github.com>
2022-08-04 19:38:29 +0530

    commited to correct the code

---
+++ lib/models/auth.dart
@@ -0,0 +1,55 @@
+import 'dart:convert';
+import 'package:fantasy_gaming/generated/json/base/json_field.dart';
+import 'package:fantasy_gaming/generated/json/auth_entity.g.dart';
+
+@JsonSerializable()
+class Auth {
+
+	String? message;
+	List<AuthData>? data;
+	bool? error;
+  
+  Auth();
+
+  factory Auth.fromJson(Map<String, dynamic> json) => $AuthEntityFromJson(json);
+
+  Map<String, dynamic> toJson() => $AuthEntityToJson(this);
+
+  @override
+  String toString() {
+    return jsonEncode(this);
+  }
+}
+
+@JsonSerializable()
+class AuthData {
+
+	String? email;
+	@JSONField(name: "device_type")
+	dynamic deviceType;
+	@JSONField(name: "device_token")
+	String? deviceToken;
+	@JSONField(name: "login_type")
+	dynamic loginType;
+	String? name;
+	String? profile;
+	@JSONField(name: "user_name")
+	String? userName;
+	@JSONField(name: "mobile_number")
+	String? mobileNumber;
+	@JSONField(name: "social_id")
+	String? socialId;
+	@JSONField(name: "api_token")
+	String? apiToken;
+  
+  AuthData();
+
+  factory AuthData.fromJson(Map<String, dynamic> json) => $AuthDataFromJson(json);
+
+  Map<String, dynamic> toJson() => $AuthDataToJson(this);
+
+  @override
+  String toString() {
+    return jsonEncode(this);
+  }
+}
