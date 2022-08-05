7b7d9d3d859ae49824abe7e258c993ba4292368c
Ritvik_Pramod <103210186+Ritvikadoor@users.noreply.github.com>
2022-08-05 23:57:08 +0530

    errorr solved

--- lib/models/auth.dart
+++ lib/models/auth.dart
@@ -1,55 +1,53 @@
-import 'dart:convert';
-import 'package:fantasy_gaming/generated/json/base/json_field.dart';
-import 'package:fantasy_gaming/generated/json/auth_entity.g.dart';
+// import 'dart:convert';
 
-@JsonSerializable()
-class Auth {
+// @JsonSerializable()
+// class Auth {
 
-	String? message;
-	List<AuthData>? data;
-	bool? error;
+// 	String? message;
+// 	List<AuthData>? data;
+// 	bool? error;
   
-  Auth();
+//   Auth();
 
-  factory Auth.fromJson(Map<String, dynamic> json) => $AuthEntityFromJson(json);
+//   factory Auth.fromJson(Map<String, dynamic> json) => $AuthEntityFromJson(json);
 
-  Map<String, dynamic> toJson() => $AuthEntityToJson(this);
+//   Map<String, dynamic> toJson() => $AuthEntityToJson(this);
 
-  @override
-  String toString() {
-    return jsonEncode(this);
-  }
-}
+//   @override
+//   String toString() {
+//     return jsonEncode(this);
+//   }
+// }
 
-@JsonSerializable()
-class AuthData {
+// @JsonSerializable()
+// class AuthData {
 
-	String? email;
-	@JSONField(name: "device_type")
-	dynamic deviceType;
-	@JSONField(name: "device_token")
-	String? deviceToken;
-	@JSONField(name: "login_type")
-	dynamic loginType;
-	String? name;
-	String? profile;
-	@JSONField(name: "user_name")
-	String? userName;
-	@JSONField(name: "mobile_number")
-	String? mobileNumber;
-	@JSONField(name: "social_id")
-	String? socialId;
-	@JSONField(name: "api_token")
-	String? apiToken;
+// 	String? email;
+// 	@JSONField(name: "device_type")
+// 	dynamic deviceType;
+// 	@JSONField(name: "device_token")
+// 	String? deviceToken;
+// 	@JSONField(name: "login_type")
+// 	dynamic loginType;
+// 	String? name;
+// 	String? profile;
+// 	@JSONField(name: "user_name")
+// 	String? userName;
+// 	@JSONField(name: "mobile_number")
+// 	String? mobileNumber;
+// 	@JSONField(name: "social_id")
+// 	String? socialId;
+// 	@JSONField(name: "api_token")
+// 	String? apiToken;
   
-  AuthData();
+//   AuthData();
 
-  factory AuthData.fromJson(Map<String, dynamic> json) => $AuthDataFromJson(json);
+//   factory AuthData.fromJson(Map<String, dynamic> json) => $AuthDataFromJson(json);
 
-  Map<String, dynamic> toJson() => $AuthDataToJson(this);
+//   Map<String, dynamic> toJson() => $AuthDataToJson(this);
 
-  @override
-  String toString() {
-    return jsonEncode(this);
-  }
-}
+//   @override
+//   String toString() {
+//     return jsonEncode(this);
+//   }
+// }
--- lib/screens/login/login_binding.dart
+++ lib/screens/login/login_binding.dart
@@ -1,14 +1,14 @@
-import 'package:fantasy_gaming/screens/login/login_controller.dart';
-import 'package:get/get.dart';
+// import 'package:fantasy_gaming/screens/login/login_controller.dart';
+// import 'package:get/get.dart';
 
 
-class LoginBinding extends Bindings{
+// class LoginBinding extends Bindings{
 
-  @override
-  void dependencies() {
-    Get.lazyPut<LoginController>(
-          () => LoginController(),
-    );
-  }
+//   @override
+//   void dependencies() {
+//     Get.lazyPut<LoginController>(
+//           () => LoginController(),
+//     );
+//   }
 
-}
+// }
